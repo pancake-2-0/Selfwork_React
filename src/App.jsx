03689fc8a95a5a4091cf7238799fca94ae2d4692
Alessia La Mura <alessialamura33@gmail.com>
2026-05-19 15:04:30 +0200

    Selfwork finito

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const name = "AAAAAAAAA";
+  return (
+    <>
+      <h1 className="title">Hello World</h1>
+      <input id="test" type="text" />
+      <hr />
+      <label className="testLabel" htmlFor="test">
+        Test Label
+      </label>
+    </>
+  );
+}
+
+export default App;
